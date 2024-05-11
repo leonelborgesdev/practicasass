de3ec8ebf4ffe479d09cc502ce134bdd4bdebc2c
leonelborgesdev <leonel03t@gmail.com>
2024-05-10 20:50:17 -0400

    header scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import css from "./Header.module.scss";
 
 export const Header = () => {
-  return <div className="">Header</div>;
+  return (
+    <div className={`paddings ${css.wrapper}`}>
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <div className={css.name}>Logo</div>
+        <ul>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">Experience</a>
+          </li>
+          <li>
+            <a href="">Portofolios</a>
+          </li>
+          <li>
+            <a href="">Testimonial</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
