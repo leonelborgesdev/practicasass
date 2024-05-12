3dc847305f59cbbc6227d993b33f857b1835b1eb
leonelborgesdev <leonel03t@gmail.com>
2024-05-12 19:13:29 -0400

    body.jsx

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Body } from "./components/Body/Body";
 import { Header } from "./components/Header/Header";
 import { Landing } from "./components/Landing/Landing";
 import css from "./styles/app.module.scss";
@@ -7,6 +8,7 @@ function App() {
     <div className={`bg-primary ${css.container}`}>
       <Header />
       <Landing />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import css from "./Body.module.scss";
+
+export const Body = () => {
+  return <secction className={css.wraper}>Body</secction>;
+};
