e3abcdd288f7b43ca9cd9dbc7af32ed882175d9c
leonelborgesdev <leonel03t@gmail.com>
2024-05-11 21:33:27 -0400

    Landing.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header/Header";
+import { Landing } from "./components/Landing/Landing";
 import css from "./styles/app.module.scss";
 
 function App() {
   return (
     <div className={`bg-primary ${css.container}`}>
-      <Header></Header>
+      <Header />
+      <Landing />
     </div>
   );
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,7 +3,32 @@ import css from "./Landing.module.scss";
 export const Landing = () => {
   return (
     <section className={`paddings ${css.wrapper}`}>
-      <div className={`innerWidth ${css.container}`}>Landing</div>
+      <div className={`innerWidth ${css.container}`}>
+        <div className={css.upperElements}>
+          <span>
+            Hey Ther, <br /> I'm Hero.
+          </span>
+          <span>
+            I design beautiful simple
+            <br />
+            things, And I love whta i do
+          </span>
+        </div>
+
+        <div className={css.lowerElements}>
+          <div className={css.experience}>
+            <div className="primaryText">10</div>
+            <div className="secondaryText">
+              <div>Years</div>
+              <div>Experience</div>
+            </div>
+          </div>
+          <div className={css.certificate}>
+            <span>CERTIFIED PROFETIONAL</span>
+            <span>UI/UX DESIGNER</span>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
