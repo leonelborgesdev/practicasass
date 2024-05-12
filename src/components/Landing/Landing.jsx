22a0d229298ddb374832975e7eb0c9edc67aed2b
leonelborgesdev <leonel03t@gmail.com>
2024-05-11 21:13:05 -0400

    Landing

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,9 @@
+import css from "./Landing.module.scss";
+
+export const Landing = () => {
+  return (
+    <section className={`paddings ${css.wrapper}`}>
+      <div className={`innerWidth ${css.container}`}>Landing</div>
+    </section>
+  );
+};
