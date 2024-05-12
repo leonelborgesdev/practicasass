e15c3099f14654130203a34855ec10f13df96e95
leonelborgesdev <leonel03t@gmail.com>
2024-05-12 19:36:17 -0400

    expertis leftside

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import { projectExperience } from "../../utils/data";
 import css from "./Body.module.scss";
 
 export const Body = () => {
-  return <secction className={css.wraper}>Body</secction>;
+  return (
+    <secction className={css.wraper}>
+      <div
+        className={`paddings yPaddings flexCenter innerWidth ${css.container}`}
+      >
+        <div className={css.leftSide}>
+          {projectExperience.map((exp, i) => {
+            return (
+              <div className={css.exp} key={i}>
+                <div className="flexCenter">
+                  <exp.icon size={25} color="white" />
+                </div>
+
+                <div>
+                  <span>{exp.name}</span>
+                  <span>{exp.projects}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className={css.rightSide}></div>
+      </div>
+    </secction>
+  );
 };
