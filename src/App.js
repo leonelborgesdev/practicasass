f8544bfdcc7671980f160b58b371b16b6864f073
leonelborgesdev <leonel03t@gmail.com>
2024-05-14 10:51:35 -0400

    exp works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Body } from "./components/Body/Body";
 import { Header } from "./components/Header/Header";
 import { Landing } from "./components/Landing/Landing";
+import { Works } from "./components/Works/Works";
 import css from "./styles/app.module.scss";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Landing />
       <Body />
+      <Works />
     </div>
   );
 }
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import css from "./Works.module.scss";
+import { motion } from "framer-motion";
+import { staggerChildren } from "../../utils/motion";
 
 export const Works = () => {
   return (
-    <section className={`paddings ${css.wrapper}`}>
-      <div className={css.container}></div>
-    </section>
+    <motion.section
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}
+    >
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <span className="primaryText yPaddings">My Work Experience</span>
+      </div>
+    </motion.section>
   );
 };
