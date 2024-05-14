f5393c31ab55a08cb21e11b066a0e5994c71b154
leonelborgesdev <leonel03t@gmail.com>
2024-05-14 10:58:46 -0400

    works scss

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import css from "./Works.module.scss";
 import { motion } from "framer-motion";
 import { staggerChildren } from "../../utils/motion";
+import { workExp } from "../../utils/data";
 
 export const Works = () => {
   return (
@@ -14,6 +15,22 @@ export const Works = () => {
     >
       <div className={`flexCenter innerWidth ${css.container}`}>
         <span className="primaryText yPaddings">My Work Experience</span>
+        <div className={`flexCenter ${css.experiences}`}>
+          {workExp.map((exp, i) => {
+            return (
+              <div className={`flexCenter ${css.exp}`} key={i}>
+                <div className={css.post}>
+                  <h1>{exp.place}</h1>
+                  <p>{exp.tenure}</p>
+                </div>
+                <div className={css.role}>
+                  <h1>{exp.role}</h1>
+                  <p>{exp.detail}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </motion.section>
   );
