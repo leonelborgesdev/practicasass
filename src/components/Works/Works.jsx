54afdba57defe984d2be904c8d80a54d98db6b1f
leonelborgesdev <leonel03t@gmail.com>
2024-05-15 11:46:30 -0400

    progressbar scss

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -30,18 +30,26 @@ export const Works = () => {
               </div>
             );
           })}
-        </div>
-
-        <div className={css.progressbar}>
-          <div className={css.line}></div>
-          <div>
-            <div className={css.circle} style={{ background: "#286F6C" }}></div>
-          </div>
-          <div>
-            <div className={css.circle} style={{ background: "#F2704E" }}></div>
-          </div>
-          <div>
-            <div className={css.circle} style={{ background: "#EEC048" }}></div>
+          <div className={css.progressbar}>
+            <div className={css.line}></div>
+            <div>
+              <div
+                className={css.circle}
+                style={{ background: "#286F6C" }}
+              ></div>
+            </div>
+            <div>
+              <div
+                className={css.circle}
+                style={{ background: "#F2704E" }}
+              ></div>
+            </div>
+            <div>
+              <div
+                className={css.circle}
+                style={{ background: "#EEC048" }}
+              ></div>
+            </div>
           </div>
         </div>
       </div>
