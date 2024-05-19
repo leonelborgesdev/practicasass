0c382333c4ffb1a2ea6891d4b1fdadc9d27f588d
leonelborgesdev <leonel03t@gmail.com>
2024-05-18 20:40:30 -0400

    framerMotion

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,16 @@
 import css from "./Portfolio.module.scss";
+import { motion } from "framer-motion";
+import { fadeIn, staggerChildren } from "../../utils/motion";
 
 export const Portfolio = () => {
   return (
-    <section className={`paddings ${css.wrapper}`}>
+    <motion.section
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amoutn: 0.25 }}
+      className={`paddings ${css.wrapper}`}
+    >
       <div className={`innerWidth flexcenter ${css.container}`}>
         <div className={css.heading}>
           <div>
@@ -10,8 +18,27 @@ export const Portfolio = () => {
 
             <p>Perfect solution for digital experience</p>
           </div>
+
+          <span className="secondaryText">Explore More Works</span>
+        </div>
+        <div className={css.showCase}>
+          <motion.img
+            variants={fadeIn("up", "tween", 0.5, 0.6)}
+            src="./image"
+            alt=""
+          />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.5, 0.6)}
+            src="./image1"
+            alt=""
+          />
+          <motion.img
+            variants={fadeIn("up", "tween", 0.5, 0.6)}
+            src="./image2"
+            alt=""
+          />
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
