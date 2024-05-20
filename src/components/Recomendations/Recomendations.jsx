2c223863d10fc58cc96e949f7fe3e9cec651e67e
leonelborgesdev <leonel03t@gmail.com>
2024-05-20 10:58:15 -0400

    recomendations

---
+++ src/components/Recomendations/Recomendations.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./Recomendations.module.scss";
+
+export const Recomendations = () => {
+  return (
+    <section className={css.wrapper}>
+      <div className={css.container}></div>
+    </section>
+  );
+};
