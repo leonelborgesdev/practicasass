fb36faa13e133da4ac8cd8e59dd10dd92e1d8322
leonelborgesdev <leonel03t@gmail.com>
2024-05-23 23:26:54 -0400

    leftSide

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,18 @@ import css from "./Footer.module.scss";
 export const Footer = () => {
   return (
     <section className={css.wrapper}>
-      <div className={css.container}>Footer</div>
+      <div className={css.container}>
+        <div className={css.left}>
+          <span className="primaryText">
+            Let's make something <br /> amazing together
+          </span>
+
+          <span className="primaryText">
+            Start by <a href="mailto:leonel03t@gmail.com">saying hi</a>
+          </span>
+        </div>
+        <div className={css.right}>right</div>
+      </div>
     </section>
   );
 };
