0227db7fa4cfa9e1e2b122d2fd4ed2623f3e23cf
leonelborgesdev <leonel03t@gmail.com>
2024-05-23 23:31:18 -0400

    rightSide

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,19 @@ export const Footer = () => {
             Start by <a href="mailto:leonel03t@gmail.com">saying hi</a>
           </span>
         </div>
-        <div className={css.right}>right</div>
+        <div className={css.right}>
+          <div className={css.info}>
+            <span className="secondaryText">Information</span>
+            <p>145 Bermejo, FL 5467, BOL</p>
+          </div>
+
+          <div className={css.menu}>
+            <li>Services</li>
+            <li>Works</li>
+            <li>Notes</li>
+            <li>Experience</li>
+          </div>
+        </div>
       </div>
     </section>
   );
